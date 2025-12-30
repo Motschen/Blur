e52a5d58deb73818a2b8af906ec68a66e5f53a8b
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2025-12-30 10:39:10 +0330

    patch MixinTitleScreen

--- src/main/java/eu/midnightdust/blur/mixin/MixinTitleScreen.java
+++ src/main/java/eu/midnightdust/blur/mixin/MixinTitleScreen.java
@@ -20,8 +20,8 @@ public abstract class MixinTitleScreen extends Screen {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/TitleScreen;renderPanorama(Lnet/minecraft/client/gui/GuiGraphics;F)V"))
     private void blur$renderTitleBlur(GuiGraphics context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (BlurConfig.blurTitleScreen) {
-            Blur.updateFadeAnimation(true);
-            if (BlurInfo.canBlur(context)) this.renderBlurredBackground(/*? if > 1.21.5 {*/ context /*?} else if <= 1.21.1 {*/ /*delta *//*?}*/);
+            Blur.updateFadeAnimation(context);
+            if (Blur.canBlur(context)) this.renderBlurredBackground(/*? if > 1.21.5 {*/ context /*?} else if <= 1.21.1 {*/ /*delta *//*?}*/);
             if (BlurConfig.darkenTitleScreen) this.renderMenuBackground(context);
         }
     }
