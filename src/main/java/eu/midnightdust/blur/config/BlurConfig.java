85c46b1a1cc6d9722cd721015ef6dcd5d0a5bb14
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2025-12-30 10:25:25 +0330

    removed inverse animation curve functions from BlurConfig.Easing and fixed mathematically incorrect sine animation curve (not needed by blur perfected)

--- src/main/java/eu/midnightdust/blur/config/BlurConfig.java
+++ src/main/java/eu/midnightdust/blur/config/BlurConfig.java
@@ -74,27 +74,24 @@ public class BlurConfig extends MidnightConfig {
         // Based on https://gist.github.com/dev-hydrogen/21a66f83f0386123e0c0acf107254843
         // Thank you very much!
 
-        FLAT(x -> x,                                    x -> x),
-        SINE(x -> 1 - cos(x * PI) / 2,                  x -> sin(x * PI) / 2),
-        QUAD(x -> x * x,                                x -> 1 - (1 - x) * (1 - x)),
-        CUBIC(x -> x * x * x,                           x -> 1 - pow(1 - x, 3)),
-        QUART(x -> x * x * x * x,                       x -> 1 - pow(1 - x, 4)),
-        QUINT(x -> x * x * x * x * x,                   x -> 1 - pow(1 - x, 5)),
-        EXPO(x -> x == 0 ? 0 : pow(2, 10 * x - 10),     x -> x == 1 ? 1 : 1 - pow(2, -10 * x)),
-        CIRC(x -> 1 - sqrt(1 - pow(x, 2)),              x -> sqrt(1 - pow(x - 1, 2))),
-        BACK(x -> 2.70158 * x * x * x - 1.70158 * x * x,x -> 1 + 2.70158 * pow(x - 1, 3) + 1.70158 * pow(x - 1, 2)),
-        ELASTIC(x -> x == 0 ? 0 : x == 1 ? 1 : -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * ((2 * PI) / 3)), x -> x == 0 ? 0 : x == 1 ? 1 : pow(2, -10 * x) * sin((x * 10 - 0.75) * ((2 * PI) / 3)) + 1);
+        FLAT(x -> x),
+        SINE(x -> 0.5 - cos(x * PI) / 2),
+        QUAD(x -> x * x),
+        CUBIC(x -> x * x * x),
+        QUART(x -> x * x * x * x),
+        QUINT(x -> x * x * x * x * x),
+        EXPO(x -> x == 0 ? 0 : pow(2, 10 * x - 10)),
+        CIRC(x -> 1 - sqrt(1 - pow(x, 2))),
+        BACK(x -> 2.70158 * x * x * x - 1.70158 * x * x),
+        ELASTIC(x -> x == 0 ? 0 : x == 1 ? 1 : -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * ((2 * PI) / 3)));
 
-        final Function<Double, Number> functionIn;
-        final Function<Double, Number> functionOut;
+        final Function<Double, Number> function;
 
-        Easing(Function<Double, Number> functionIn, Function<Double, Number> functionOut) {
-            this.functionIn = functionIn;
-            this.functionOut = functionOut;
+        Easing(Function<Double, Number> function) {
+            this.function = function;
         }
-        public Double apply(Double x, boolean in) {
-            if (in) return functionIn.apply(x).doubleValue();
-            return functionOut.apply(x).doubleValue();
+        public Double apply(Double x) {
+            return function.apply(x).doubleValue();
         }
     }
 
