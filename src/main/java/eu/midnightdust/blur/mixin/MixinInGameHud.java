f3b399f28e714e202686de6da0af13c882d4c7f7
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2025-12-30 10:35:19 +0330

    patch MixinInGameHud

--- src/main/java/eu/midnightdust/blur/mixin/MixinInGameHud.java
+++ src/main/java/eu/midnightdust/blur/mixin/MixinInGameHud.java
@@ -18,17 +18,20 @@ public class MixinInGameHud {
 
     @Inject(at = @At("TAIL"), method = "render")
     public void blur$renderFadeOut(GuiGraphics context, DeltaTracker tickCounter, CallbackInfo ci) { // Adds a fade-out effect when a player is in a world and closes all screens
-        if (minecraft.screen == null && minecraft.level != null && BlurInfo.start >= 0 && BlurInfo.prevScreenHasBlur) {
-            BlurInfo.doTest = false;
-            BlurInfo.screenChanged = false;
+        if (minecraft.screen == null && minecraft.level != null) {
+            Blur.screenHasBlur = true;
+            Blur.onRender(context);
+
+            if (Blur.fadeTimeState < 0.001F) return;  // we have faded out at this point and don't need to render anything
+
             //? if > 1.21.5 {
-            if (BlurInfo.canBlur(context))
+            if (Blur.canBlur(context))
                 context.blurBeforeThisStratum();
             //?} else {
             /*minecraft.gameRenderer.processBlurEffect(/^? if <= 1.21.1 {^/ /^tickCounter.getGameTimeDeltaTicks() ^//^?}^/);
             *///?}
 
-            if (BlurInfo.prevScreenHasBackground) Blur.renderRotatedGradient(context, minecraft.getWindow().getGuiScaledWidth(), minecraft.getWindow().getGuiScaledHeight());
+            Blur.renderRotatedGradient(context, minecraft.getWindow().getGuiScaledWidth(), minecraft.getWindow().getGuiScaledHeight());
         }
     }
 }
