7197db4f23ac1e4f011870a9008aacd5e0489e78
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2026-01-07 20:10:09 +0330

    revert rebranding

--- src/main/java/eu/midnightdust/blur/Blur.java
+++ src/main/java/eu/midnightdust/blur/Blur.java
@@ -41,7 +41,7 @@ import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 
 public class Blur {
-    public static final String MOD_ID = "blurperfected";
+    public static final String MOD_ID = "blur";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static void init() {
         BlurConfig.init(MOD_ID, BlurConfig.class);
